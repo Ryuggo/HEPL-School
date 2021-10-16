828e86efd8998ee4c8cf03637957fffbcade7d69
Hugo Fr <51454951+Ryuggo@users.noreply.github.com>
2021-10-16 23:56:57 +0200

    Delete B2/Html - PhP directory

--- B2/Html - PhP/include/connectDB.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $con = mysqli_connect("localhost", "root", "", "dossier_juin_2020");
-    mysqli_query($con, "SET NAMES UTF8");
-
-    if(mysqli_connect_errno()) {
-        printf("Échec de la connexion : %s\n", mysqli_connect_error());
-        exit();
-    }
-?>
--- B2/Html - PhP/include/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="text-center">
-    <p>Examen de Juin © <?php echo(date("Y", time()-31536000)." - ".date("Y")); ?> - Mentions légales du projet</p>
-</footer>
-
-<?php
-    mysqli_close($con);
-?>
